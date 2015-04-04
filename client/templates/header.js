632c413297b77d1a6a4f6d53e0a9c58cb81d8636
iboxgithub <ibox@laposte.net>
2015-04-04 16:58:26 +0200

    first

--- client/templates/header.js
+++ client/templates/header.js
@@ -1,3 +1,15 @@
 /**
  * Created by ibox on 4/4/15.
  */
+Template.header.helpers({
+    activeRouteClass: function(/* route names */) {
+        var args = Array.prototype.slice.call(arguments, 0);
+        args.pop();
+
+        var active = _.any(args, function(name) {
+            return Router.current() && Router.current().route.getName() === name
+        });
+
+        return active && 'active';
+    }
+});
